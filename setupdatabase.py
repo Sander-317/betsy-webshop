891fa282096a8b10ce69b9ccbbe9f4373fa8a821
Sander-317 <sander.van.meerveld@gmail.com>
2023-10-04 14:07:52 +0200

    added some comments to setupdatabase.py

--- setupdatabase.py
+++ setupdatabase.py
@@ -13,6 +13,7 @@ def main():
     setup_data()
 
 
+# Read csv files
 def get_csv_data(file):
     with open(f"csv_data/{file}", "r") as user_csv:
         csv_reader = csv.reader(user_csv, delimiter=",")
@@ -22,6 +23,7 @@ def get_csv_data(file):
         return data
 
 
+# sets up the data base and fils it with data
 def setup_data():
     models.db.connect()
     models.db.create_tables(
@@ -34,14 +36,16 @@ def setup_data():
             models.Transaction,
         ]
     )
-
+    # create users
     [
         models.User.create(name=user[0], address=user[1], billing_info=user[2])
         for user in get_csv_data("users.csv")
     ]
 
+    # finds total amount of users
     number_of_users = len(models.User.select())
 
+    # creates the product
     for product in get_csv_data("product.csv"):
         new_product = models.Product.create(
             name=product[0],
@@ -49,7 +53,7 @@ def setup_data():
             price_in_cents=product[2],
             quantity=product[3],
         )
-
+        # creates the tags and checks for duplicates
         for i in product[4].split():
             current_tags = []
             [current_tags.append(i.name) for i in models.Tag.select(models.Tag.name)]
@@ -57,11 +61,15 @@ def setup_data():
                 models.Tag.create(name=i)
             new_product.tag.add(models.Tag.get(models.Tag.name == i).id)
 
+        # assings owner to the product
         owner = models.User.get(models.User.id == random.randint(1, number_of_users))
         owner.owned_products.add(models.Product.get(models.Product.name == product[0]))
         owner.save()
 
+    # find total amount of products
     number_of_products = len(models.Product.select())
+
+    # creates some transactions
     for i in range((number_of_users + number_of_products) * 2):
         new_buyer = random.randint(1, number_of_users)
         new_product = random.randint(1, number_of_products)
@@ -72,11 +80,13 @@ def setup_data():
             quantity=bought_quantity,
         )
 
+    # corrects the inventory
     product = models.Product.get(models.Product.id == new_product)
     product.quantity = product.quantity - bought_quantity
     product.save()
 
 
+# deletes the data base
 def delete_database():
     cwd = os.getcwd()
     database_path = os.path.join(cwd, "betsy-database.db")
